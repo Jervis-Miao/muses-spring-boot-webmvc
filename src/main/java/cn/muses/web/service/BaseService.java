053327e3848d49b33bdafb4bd3de0d1638a899d7
miaoqiang <miaoqiang@xyz.cn>
2023-09-13 14:56:48 +0800

    添加基础Service类

---
+++ src/main/java/cn/muses/web/service/BaseService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2021 XYZ Co., Ltd. All Rights Reserved
+ */
+package cn.muses.web.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author miaoqiang
+ * @date 2023/9/13.
+ */
+public class BaseService {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+}
