80b06a21574e4ae269adc664510f167dce6e364e
jervis <jervis_qiang@aliyun.com>
2024-08-26 11:48:37 +0800

    aliyun mpaas package

---
+++ src/main/java/xyz/muses/external/aliyun/mpaas/MpaasDemo.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2024 xyz Co., Ltd. All Rights Reserved
+ */
+package xyz.muses.external.aliyun.mpaas;
+
+/**
+ * @author jervis
+ * @date 2024/8/26
+ */
+public class MpaasDemo {
+}
