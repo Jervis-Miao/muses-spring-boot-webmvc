45eabb25f2ba3855c2078fa90dd23613ba4d973b
jervis <jervis_qiang@aliyun.com>
2024-08-26 15:42:14 +0800

    RedissonSingleConfig

--- src/main/java/xyz/muses/config/RedissonSentinelConfig.java
+++ src/main/java/xyz/muses/config/RedissonSentinelConfig.java
@@ -9,25 +9,51 @@ import java.util.stream.Collectors;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.Codec;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.FstCodec;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.codec.SerializationCodec;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import xyz.muses.config.redis.RedissonProperties;
 import xyz.muses.config.redis.SentinelProperties;
+import xyz.muses.config.redis.SingleProperties;
 
 /**
  * @author jervis
  * @date 2020/12/1.
  */
 @Configuration
-@EnableConfigurationProperties({SentinelProperties.class, RedissonProperties.class})
+@EnableConfigurationProperties({SentinelProperties.class, SingleProperties.class, RedissonProperties.class})
 public class RedissonSentinelConfig {
 
-    @Bean("redissonClient")
-    public RedissonClient initRedisson(@Qualifier("fstCodec") Codec codec, SentinelProperties sentinelProp,
+    @Bean("fstCodec")
+    public FstCodec fstCodec() {
+        return new FstCodec();
+    }
+
+    @Bean("jsonJacksonCodec")
+    public JsonJacksonCodec jsonJacksonCodec() {
+        return new JsonJacksonCodec();
+    }
+
+    @Bean("jdkCodec")
+    public SerializationCodec jdkCodec() {
+        return new SerializationCodec();
+    }
+
+    @Bean("StringCodec")
+    public StringCodec stringCodec() {
+        return new StringCodec();
+    }
+
+    @Bean("sentinelRedissonClient")
+    public RedissonClient sentinelRedisson(@Qualifier("fstCodec") Codec codec, SentinelProperties sentinelProp,
         RedissonProperties redissonProp) {
         Config config = new Config();
         config.useSentinelServers()
@@ -43,6 +69,28 @@ public class RedissonSentinelConfig {
             .setMasterConnectionPoolSize(sentinelProp.getMasterConnectionPoolSize())
             .setFailedSlaveReconnectionInterval(sentinelProp.getFailedSlaveReconnectionInterval())
             .setFailedSlaveCheckInterval(sentinelProp.getFailedSlaveCheckInterval())
+            .setIdleConnectionTimeout(redissonProp.getIdleConnectionTimeout())
+            .setConnectTimeout(redissonProp.getConnectTimeout())
+            .setTimeout(redissonProp.getTimeout())
+            .setRetryAttempts(redissonProp.getRetryAttempts())
+            .setRetryInterval(redissonProp.getRetryInterval())
+            .setSubscriptionConnectionMinimumIdleSize(redissonProp.getSubscriptionConnectionMinimumIdleSize())
+            .setSubscriptionConnectionPoolSize(redissonProp.getSubscriptionConnectionPoolSize())
+            .setSubscriptionsPerConnection(redissonProp.getSubscriptionsPerConnection())
+            .setDatabase(redissonProp.getDatabase())
+            .setPassword(redissonProp.getPassword());
+        // 序列化方式
+        config.setCodec(codec);
+        return Redisson.create(config);
+    }
+
+    @Bean("singleRedissonClient")
+    @ConditionalOnMissingBean(name = "sentinelRedissonClient")
+    public RedissonClient singleRedisson(@Qualifier("fstCodec") Codec codec, SingleProperties singleProp,
+        RedissonProperties redissonProp) {
+        Config config = new Config();
+        config.useSingleServer()
+            .setAddress("redis://" + singleProp.getHost() + ":" + singleProp.getPort())
             .setIdleConnectionTimeout(redissonProp.getIdleConnectionTimeout())
             .setConnectTimeout(redissonProp.getConnectTimeout())
             .setTimeout(redissonProp.getTimeout())
